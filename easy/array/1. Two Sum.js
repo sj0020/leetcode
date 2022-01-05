77bb865462652d2cc393eb10c51e801101422365
sj0020 <62295464+sj0020@users.noreply.github.com>
2022-01-05 19:50:00 +0900

    Update 1. Two Sum.js

--- easy/array/1. Two Sum.js
+++ easy/array/1. Two Sum.js
@@ -5,8 +5,7 @@ var twoSum = function(nums, target) {
               for (j = i+1; j< arrlength; j++){
                   if(nums[i] + nums[j] === target){
                       var returnArr = [];
-                      returnArr.push(i);
-                      returnArr.push(j);
+                      returnArr.push(i,j);
                       // console.log(returnArr);
                       return returnArr;
                     }                    
