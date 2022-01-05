4e961311486ccc202bd1d2a8d70eebd3abf622d3
sj0020 <62295464+sj0020@users.noreply.github.com>
2022-01-05 19:20:58 +0900

    Create 1. Two Sum.js
    처음에   var returnArr = []; 을 함수밖에 선언했다가  계속 run code 하고  console.log 에 찍으면 정답이 나오는데 submit 하면 답이 틀리다 나와서  왜그런가 했는데 이 이유였다.
    
    https://leetcode.com/discuss/feedback/1104251/wrong-answer-during-submission-is-correct-when-using-run-code

---
+++ easy/array/1. Two Sum.js
@@ -0,0 +1,17 @@
+var twoSum = function(nums, target) {
+          var arrlength = nums.length;
+          // console.log(arrlength);
+          for (i = 0; i< arrlength; i++){
+              for (j = i+1; j< arrlength; j++){
+                  if(nums[i] + nums[j] === target){
+                      var returnArr = [];
+                      returnArr.push(i);
+                      returnArr.push(j);
+                      // console.log(returnArr);
+                      return returnArr;
+                    }                    
+              }
+          }
+      };
+
+
